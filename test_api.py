0d9b0e890ddb6d8493cd2751e7bf6548bb1466df
Pratyush Singh Naruka <142869076+pratyushsinghnaruka@users.noreply.github.com>
2025-03-05 12:14:47 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-api_url = "https://threats-detection.onrender.com"  # Change this if API is deployed online
+api_url = "https://threat-detection-ht6b.onrender.com"  # Change this if API is deployed online
 test_data = {"url": "http://malicious-site.com"}
 
 response = requests.post(api_url, json=test_data)
